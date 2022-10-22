db70beaf7fcff5cce144c2864ee93ac9dcae99cd
Chelsea Dwarika <chelseakristina@live.com>
2022-10-22 01:56:29 +0000

    added newTimer function to make code more reusable and modular!

--- timer1.js
+++ timer1.js
@@ -1,12 +1,20 @@
-  
-  const beep = function () {
+//fn beep helper function used to create sound in node.js
+const beep = function() {
   process.stdout.write('\x07');
-  }
+};
+//loop through arguments passed in node representing the seconds that the timer sound will execute
+//process.argv starts at [2]
+//arguments will NOT pass if they are not numbers/if they are negative numbers
 
+const newTimer = function() {
   for (let i = 2; i < process.argv.length; i++) {
     if ((process.argv[i] < 0) || (isNaN(process.argv[i]))) {
       return false;
     } else {
-    setTimeout(beep, (process.argv[i] * 1000));
+//setTimeout callback of beep function, * 1000 depending on process.argv argument in milliseconds
+      setTimeout(beep, (process.argv[i] * 1000));
+    }
   }
 };
+
+newTimer();
